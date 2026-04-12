341d4a736bed3c5d3dd296dbc598e2fab660ecba
Marcin Majborski <marcinmajborski@gmail.com>
2026-04-12 23:38:01 +0200

    naprawa scrapowania cen

--- orlen_project/spiders/hurtowe_orlen.py
+++ orlen_project/spiders/hurtowe_orlen.py
@@ -1,7 +1,7 @@
 import scrapy
 import json
 import os
-import csv  # <-- DODANY IMPORT do samodzielnego zapisu pliku
+import csv
 from datetime import datetime, timedelta
 
 class HurtoweOrlenSpider(scrapy.Spider):
@@ -14,15 +14,13 @@ class HurtoweOrlenSpider(scrapy.Spider):
 
     paliwa_id = [41,42,43]
 
-    # Inicjalizujemy pustą listę, w której przechowamy wszystkie pobrane ceny
     def __init__(self, *args, **kwargs):
         super(HurtoweOrlenSpider, self).__init__(*args, **kwargs)
         self.zebrane_dane = []
 
     def start_requests(self):
         dzisiaj = datetime.now()
-        # ZMIANA: Zawsze pobieramy dane z ostatnich 7 dni. 
-        # Dzięki temu nawet jak nie włączysz komputera przez 3 dni, skrypt nadrobi zaległości!
+        # Skrypt pobiera dane tylko z ostatnich 7 dni
         siedem_dni_temu = dzisiaj - timedelta(days=7)
         
         data_od = siedem_dni_temu.strftime('%Y-%m-%d')
@@ -48,38 +46,44 @@ class HurtoweOrlenSpider(scrapy.Spider):
             surowa_data = item.get('effectiveDate', '')
             czysta_data = surowa_data.split('T')[0] if 'T' in surowa_data else surowa_data
 
-            # Zamiast 'yield', DODAJEMY dane do naszej listy w pamięci
             self.zebrane_dane.append({
                 'data': czysta_data,
                 'paliwo': nazwa_paliwa,
                 'cena_netto_pln_m3': item.get('value')
             })
 
-    # Ta metoda uruchamia się automatycznie TYLKO RAZ, na samym końcu działania skrapera
     def closed(self, reason):
         nazwa_pliku = 'data/ceny_orlen_zestawienie.csv'
-        wszystkie_dane = []
-        
-        # 1. Wczytujemy historię z OneDrive (jeśli plik istnieje)
+        istniejace_klucze = set()
+
+        # 1. Zbieramy klucze z już istniejącego pliku (żeby uniknąć duplikatów przy dopisywaniu)
         if os.path.isfile(nazwa_pliku):
             with open(nazwa_pliku, mode='r', encoding='utf-8') as plik:
                 reader = csv.DictReader(plik)
-                wszystkie_dane = list(reader)
+                for row in reader:
+                    istniejace_klucze.add((row['data'], row['paliwo']))
 
-        # 2. Dodajemy nowo pobrane dni
-        wszystkie_dane.extend(self.zebrane_dane)
+        # 2. Wybieramy TYLKO te dane, których jeszcze nie ma w starym pliku
+        nowe_dane = []
+        for d in self.zebrane_dane:
+            klucz = (d['data'], d['paliwo'])
+            if klucz not in istniejace_klucze:
+                nowe_dane.append(d)
+                # Zapobiega duplikatom w samej nowej pobranej paczce
+                istniejace_klucze.add(klucz) 
 
-        # 3. MAGIA: Usuwamy duplikaty. 
-        # Używamy kombinacji (data + paliwo) jako unikalnego klucza. Słownik sam nadpisze duplikaty!
-        unikalne = {(d['data'], d['paliwo']): d for d in wszystkie_dane}
-        
-        # Sortujemy chronologicznie
-        unikalne_lista = sorted(unikalne.values(), key=lambda x: x['data'], reverse=True)
-
-        # 4. Zapisujemy zaktualizowany, czysty plik (mode='w', bo nadpisujemy całość od nowa)
-        with open(nazwa_pliku, mode='w', encoding='utf-8', newline='') as plik:
-            writer = csv.DictWriter(plik, fieldnames=['data', 'paliwo', 'cena_netto_pln_m3'])
-            writer.writeheader()
-            writer.writerows(unikalne_lista)
-            
-        self.logger.info(f"Zaktualizowano bazę. Plik ma teraz {len(unikalne_lista)} unikalnych rekordów.")
+        # 3. Jeśli mamy coś nowego, dopisujemy na sam dół pliku (mode='a')
+        if nowe_dane:
+            plik_istnieje = os.path.isfile(nazwa_pliku)
+            with open(nazwa_pliku, mode='a', encoding='utf-8', newline='') as plik:
+                writer = csv.DictWriter(plik, fieldnames=['data', 'paliwo', 'cena_netto_pln_m3'])
+                
+                # Jeśli plik w ogóle nie istniał, najpierw dajemy nagłówki
+                if not plik_istnieje:
+                    writer.writeheader()
+                    
+                writer.writerows(nowe_dane)
+                
+            self.logger.info(f"✅ Dopisano {len(nowe_dane)} nowych rekordów do bazy.")
+        else:
+            self.logger.info("ℹ️ Brak nowych cen. Baza jest aktualna.")
